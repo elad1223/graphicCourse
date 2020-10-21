1566cd22e071bb7512bee7f02e5c104d023bdbc5
Elad Gershon <elad15973@gmail.com>
2020-10-21 19:35:17 +0300

    'ex0'

---
+++ Assets/EX0/Assets/GameController.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameController : MonoBehaviour
+{
+    public static int FIELD_SIZE = 30; // Width and height of the game field
+    public static float COLLISION_THRESHOLD = 1.5f; // Collision distance between food and player 
+    public GameObject playerObject, camera; // Reference to the Player GameObject
+
+    private GameObject food; // Represents the food in the game
+    private int score = 0;
+    private Vector3 offset;
+    private Transform foodT, playerT;
+    // Start is called before the first frame update
+    void Start()
+    {
+        food = GameObject.CreatePrimitive(PrimitiveType.Cylinder);
+        foodT = food.GetComponent<Transform>();
+        playerT = playerObject.GetComponent<Transform>();
+        offset =  camera.transform.position - playerObject.transform.position;
+        SpawnFood();
+    }
+
+    // Positions the food at a random location inside the field
+    void SpawnFood()
+    {
+        food.transform.position = new Vector3(Random.Range(-FIELD_SIZE/2, FIELD_SIZE/2),
+                                               1,
+                                               Random.Range(-FIELD_SIZE/2, FIELD_SIZE/2));
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Vector3.Distance(foodT.position, playerT.position) < COLLISION_THRESHOLD)
+        {
+            SpawnFood();
+            score++;
+            Debug.Log(score);
+        }
+        camera.transform.position = playerT.position + offset;
+    }
+}
---
+++ Assets/EX0/Assets/PlayerController.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    public float movementForce = 500; // Controls player movement power
+    private Rigidbody body;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        body = GetComponent<Rigidbody>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.anyKey)
+        {
+            Vector3 dir = new Vector3();
+            if (Input.GetKey(KeyCode.RightArrow))
+                dir = dir + Vector3.right;
+            if (Input.GetKey(KeyCode.LeftArrow))
+                dir = dir - Vector3.right;
+            if (Input.GetKey(KeyCode.UpArrow))
+                dir = dir + Vector3.forward;
+            if (Input.GetKey(KeyCode.DownArrow))
+                dir = dir - Vector3.forward;
+            body.AddForce(dir*Time.deltaTime* movementForce);
+        }
+        if (transform.position.y < -50)
+            transform.position = new Vector3(0, 1, 0);
+        // Implement movement logic here
+    }
+}
