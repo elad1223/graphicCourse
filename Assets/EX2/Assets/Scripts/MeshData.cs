1b31160cf936a53765ef7912647c7645d155e340
Elad Gershon <elad15973@gmail.com>
2020-11-23 15:19:10 +0200

    part1-meshData

--- Assets/EX2/Assets/Scripts/MeshData.cs
+++ Assets/EX2/Assets/Scripts/MeshData.cs
@@ -33,12 +33,38 @@ public class MeshData
     // Calculates surface normals for each vertex, according to face orientation
     public void CalculateNormals()
     {
-        // Your implementation
+        normals = new Vector3[vertices.Count];
+        //foreach tringle
+        for (int i = 0; i < triangles.Count; i += 3)
+        {
+            Vector3 a=vertices[triangles[i]],
+                b= vertices[triangles[i+1]],
+                c=vertices[triangles[i+2]];
+            Vector3 normal_surface = Vector3.Cross(a - c, b - c).normalized;
+            //adding the normals to each vertex that build it
+            for(int j=i;j<i+3;j++)
+                normals[triangles[j]] = normals[triangles[j]] + normal_surface;
+        }
+        //normalize the normals
+        for (int i = 0; i < normals.Length; i++)
+            normals[i] = normals[i].normalized;
     }
 
     // Edits mesh such that each face has a unique set of 3 vertices
     public void MakeFlatShaded()
     {
+        HashSet<int> seen = new HashSet<int>();
+        for(int i = 0; i < triangles.Count; i++)
+        {
+            int index = triangles[i];
+            if (seen.Contains(index))
+            {
+                Vector3 to_add = vertices[triangles[i]];
+                vertices.Add(to_add);
+                triangles[i] = vertices.Count - 1;
+            }
+            else { seen.Add(index); }
+        }
         // Your implementation
     }
 }
